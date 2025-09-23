ff1c8aa78cf3a7174b17691fc9d1dc576b3f0ae4
Vamsi Ravula <codingsaduvu@gmail.com>
2025-09-23 15:43:47 -0700

    Add Consumer-4

--- demo-cg_consumers.sh
+++ demo-cg_consumers.sh
@@ -9,9 +9,9 @@ TOPIC=demo
 GROUP=demo-cg
 
 echo "[cleanup] deleting any old consumers..."
-oc delete pod cg-c1 cg-c2 cg-c3 --ignore-not-found >/dev/null 2>&1 || true
+oc delete pod cg-c1 cg-c2 cg-c3 cg-c4 --ignore-not-found >/dev/null 2>&1 || true
 
-echo "[1/3] starting the first consumer cg-c1..."
+echo "[1/4] starting the first consumer cg-c1..."
 oc run cg-c1 --restart=Never \
   --image="$IMAGE" -- \
   bash -lc "/opt/kafka/bin/kafka-console-consumer.sh \
@@ -28,7 +28,7 @@ oc wait pod/cg-c1 --for=condition=Ready --timeout=120s
 
 prompt $'[input] Press Enter to start the second consumer cg-c2...'
 
-echo "[2/3] starting consumer cg-c2 in the same group ..."
+echo "[2/4] starting consumer cg-c2 in the same group ..."
 oc run cg-c2 --restart=Never \
   --image="$IMAGE" -- \
   bash -lc "/opt/kafka/bin/kafka-console-consumer.sh \
@@ -43,7 +43,7 @@ oc wait pod/cg-c2 --for=condition=Ready --timeout=120s
 
 prompt $'[input] Press Enter to start the third consumer cg-c3...'
 
-echo "[3/3] starting consumer cg-c3 in the same group..."
+echo "[3/4] starting consumer cg-c3 in the same group..."
 oc run cg-c3 --restart=Never \
   --image="$IMAGE" -- \
   bash -lc "/opt/kafka/bin/kafka-console-consumer.sh \
@@ -57,8 +57,30 @@ oc run cg-c3 --restart=Never \
 oc wait pod/cg-c3 --for=condition=Ready --timeout=120s
 
 echo
-echo "✅ All three consumers are running and consuming messages"
+echo "✅ All three consumers are running and have been assigned partitions"
+
+prompt $'[input] Press Enter to start the fourth consumer cg-c4...'
+
+echo "[4/4] starting consumer cg-c4 in the same group..."
+oc run cg-c4 --restart=Never \
+  --image="$IMAGE" -- \
+  bash -lc "/opt/kafka/bin/kafka-console-consumer.sh \
+    --bootstrap-server $BOOTSTRAP \
+    --topic $TOPIC \
+    --group $GROUP \
+    --consumer-property client.id=\$HOSTNAME \
+    --property print.key=true \
+    --property print.partition=true \
+    --property key.separator=' | '"
+oc wait pod/cg-c4 --for=condition=Ready --timeout=120s
+
 echo
-prompt $'[input] Press Enter to STOP all consumers...'
-oc delete pod cg-c1 cg-c2 cg-c3 --now --ignore-not-found
-echo "🛑 Stopped."
+echo "✅ Consumer 4 is running and but is currently idle"
+echo
+prompt $'[input] Press Enter to STOP all consumers except cg-c1...'
+oc delete pod cg-c2 cg-c3 cg-c4 --now --ignore-not-found
+echo "🛑 Stopped cg-c2, cg-c3, cg-c4. cg-c1 remains running."
+
+prompt $'[input] Press Enter to STOP the remaining consumer cg-c1...'
+oc delete pod cg-c1 --now --ignore-not-found
+echo "🛑 All consumers stopped."
